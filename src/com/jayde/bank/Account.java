4a81a76177fff7570c99b7829047c6c31b27de3d
jayde-code <juhnmayer@gmail.com>
2021-05-22 02:31:54 +0900

    first commit

---
+++ src/com/jayde/bank/Account.java
@@ -0,0 +1,46 @@
+package com.jayde.bank;
+
+public class Account {
+    private String name;
+    private String acc_no;
+    private int balance;
+
+    public Account(String name, String acc_no, int balance) {
+        this.name = name;
+        this.acc_no = acc_no;
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "name='" + name + '\'' +
+                ", acc_no='" + acc_no + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAcc_no() {
+        return acc_no;
+    }
+
+    public void setAcc_no(String acc_no) {
+        this.acc_no = acc_no;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+}
---
+++ src/com/jayde/bank/Main.java
@@ -0,0 +1,78 @@
+package com.jayde.bank;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    private static final ArrayList<Account> acc_list = new ArrayList();
+    private static final Scanner sc = new Scanner(System.in);
+
+    private static void createAcc() {
+//        계좌번호 랜덤 생성
+        String acc_no = String.format("%03d", (int)(Math.random() * 1000) - 1) + "-"
+                + String.format("%03d", (int)(Math.random() * 1000 - 1));
+        System.out.print("name? ");
+        String name = sc.nextLine();
+        System.out.print("balance? ");
+        int balance = sc.nextInt();
+
+        acc_list.add(new Account(name, acc_no, balance));
+    }
+
+    private static void readAcc() {
+        for (Account i : acc_list) {
+            System.out.println(i.toString());
+        }
+    }
+
+    private static void deposit() {
+        for (Account i : acc_list) {
+            System.out.println("|" + acc_list.indexOf(i) + "|" + i.toString());
+        }
+        System.out.print("입금할 계좌의 인덱스 번호? ");
+        int index_no = sc.nextInt();
+        System.out.print("입금할 금액 : ");
+        int dep = sc.nextInt();
+        for (Account i : acc_list) {
+            if (acc_list.indexOf(i) == index_no) {
+                i.setBalance(i.getBalance() + dep);
+                System.out.println("예금 성공");
+                break;
+            }
+        }
+    }
+
+    private static void withdraw() {
+        for (Account i : acc_list) {
+            System.out.println("|" + acc_list.indexOf(i) + "|" + i.toString());
+        }
+        System.out.print("출금할 계좌의 인덱스 번호? ");
+        int index_no = sc.nextInt();
+        System.out.print("출금할 금액 : ");
+        int with = sc.nextInt();
+        for (Account i : acc_list) {
+            if (i.getBalance() >= with && acc_list.indexOf(i) == index_no) {
+                i.setBalance(i.getBalance() - with);
+                System.out.println("출금 성공");
+            } else {
+                System.out.println("출금하려는 금액이 예금액보다 적습니다.");
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("===== Jay BANK =====");
+        while (true) {
+            System.out.println("1Create 2Read 3Deposit 4Withdraw 0EXIT");
+            System.out.print("INPUT>> ");
+            String input_no = sc.nextLine();
+
+            if (input_no.equals("1")) createAcc();
+            else if (input_no.equals("2")) readAcc();
+            else if (input_no.equals("3")) deposit();
+            else if (input_no.equals("4")) withdraw();
+            else if (input_no.equals("0")) break;
+        }
+    }
+}
